095bb2c9cbfd74c360886462da28e7f35c5cb0df
Yuri Nudelman <yuri@excelero.com>
2019-09-01 15:39:06 +0300

    Tmux synchronize panes keyboard shortcut

--- tmux.conf.bash
+++ tmux.conf.bash
@@ -39,3 +39,6 @@ bind-key -T copy-mode 'y' send-keys -X copy-pipe-and-cancel 'xclip -i -f -select
 
 # Save tmux buffer
 bind-key P command-prompt -p 'save history to filename:' -I '~/tmux.history' 'capture-pane -S -32768 ; save-buffer %1 ; delete-buffer'
+
+# Synchronize panes
+bind-key e setw synchronize-panes
